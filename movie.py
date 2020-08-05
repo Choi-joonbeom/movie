6b5edd03e01c2f391f9539d187d1efebbfd4dbe0
Choi-joonbeom <66134805+Choi-joonbeom@users.noreply.github.com>
2020-08-05 14:37:53 +0900

    Update movie.py

--- movie.py
+++ movie.py
@@ -5,8 +5,7 @@ from bs4 import BeautifulSoup
 response = requests.get('https://movie.naver.com/movie/running/current.nhn')
 soup = BeautifulSoup(res_pages.content,'html.parser')
 
-movies_list = soup.select('#content > .article > .obj_section >
- .lst_wrap > ul > li')
+movies_list = soup.select('#content > .article > .obj_section > .lst_wrap > ul > li')
  final_movie_data = []
  for movie in movies_list :
      a_tag = movie.select_one('dl > dt > a')
